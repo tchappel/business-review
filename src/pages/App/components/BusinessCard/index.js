110cbba054da7d1af31669e7e496e6c8bb662a66
trc485 <thomas.chappel4@gmail.com>
2018-11-12 22:04:37 +0100

    fix errors

--- src/pages/App/components/BusinessCard/index.js
+++ src/pages/App/components/BusinessCard/index.js
@@ -6,11 +6,43 @@ class BusinessCard extends Component {
     render() { 
         return ( 
             <div className="card">
-            {/*
+            {/* 
+                card-deck {
+                display: flex;
+                flex-direction: column;
+                }
+
+                @media (min-width: 576px)
+                .card-deck {
+                    -ms-flex-flow: row wrap;
+                    flex-flow: row wrap;
+                    margin-right: -15px;
+                    margin-left: -15px;
+                }
+                
+
+                @media (min-width: 576px) {
                 display: flex;
                 flex: 1 0 0%;
                 flex-direction: column;
                 position: relative;
+                flex-direction: column;
+                margin-right: 15px;
+                margin-bottom: 0;
+                margin-left: 15px;
+                }
+
+                .card {
+                    position: relative;
+                    display: flex;
+                    flex-direction: column;
+                    min-width: 0;
+                    word-wrap: break-word;
+                    background-color: #fff;
+                    background-clip: border-box;
+                    border: 1px solid rgba(0, 0, 0, 0.125);
+                    border-radius: 0.25rem;
+                }
             */}
                 <div className="card-body d-flex flex-row">
                 {/*
@@ -39,7 +71,7 @@ class BusinessCard extends Component {
                         }
                         {
                             !!this.props.businessCategory && 
-                            <p>Category: {this.props.businessRating}</p>
+                            <p>Category: {this.props.businessCategory}</p>
                         }
                     </div>                    
                 </div>
