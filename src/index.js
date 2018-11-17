8a477b70e7c3d939aa6328f136fb9e9d9bba261e
trc485 <thomas.chappel4@gmail.com>
2018-11-17 13:21:58 +0100

    install react-router-dom and initial config

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { BrowserRouter } from "react-router-dom";
 // import $ from "jquery";
 // import Popper from "popper.js";
 import "bootstrap/dist/js/bootstrap.bundle.min";
@@ -7,8 +8,14 @@ import App from "./App";
 import "./index.css";
 
 import * as serviceWorker from "./serviceWorker";
+import { from } from "rxjs";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+ReactDOM.render(
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>,
+  document.getElementById("root")
+);
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: http://bit.ly/CRA-PWA
